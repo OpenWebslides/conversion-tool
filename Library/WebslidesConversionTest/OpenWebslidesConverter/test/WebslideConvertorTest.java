6a087386688cf57e3452f4f4b851223db56f707d
kavdnhou <karel.vandenhoute@ugent.be>
2017-03-09 11:49:28 +0100

    Unit test

---
+++ Library/WebslidesConversionTest/OpenWebslidesConverter/test/WebslideConvertorTest.java
@@ -0,0 +1,61 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import conversion.ConverterFactory;
+import conversion.IConverter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import objects.PPT;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Karel
+ */
+public class WebslideConvertorTest {
+    
+    public WebslideConvertorTest() {
+    }
+    
+
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    
+    
+    @Test
+    public void testRun() {
+        File file = new File("C:\\temp\\ppt.pptx");
+        
+        IConverter converter = ConverterFactory.getConverter(file);
+        
+        PPT ppt = new PPT(new File(""));
+        
+        converter.parse(ppt);
+        
+	FileWriter fw = null;
+
+        try {
+                fw = new FileWriter("C:\\temp\\test.html");
+                fw.write(ppt.toHTML());
+        } catch (Exception e) {
+        } finally {
+            try {
+                fw.close();
+            } catch (IOException ex) {
+            }
+        }
+
+        
+    }
+}
