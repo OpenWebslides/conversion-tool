3dff5be29323756fb3ed273aa5dc90a9355aa7b8
Joann Chan <joann.chan@ugent.be>
2017-03-18 23:46:48 +0100

    basic page with dragndrop file opload and extra options

---
+++ WebSlidesWebapp/web/resources/js/dropzone_options.js
@@ -0,0 +1,41 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+Dropzone.options.myAwesomeDropzone = {
+    paramName: "file", // The name that will be used to transfer the file
+    maxFilesize: 10000, // MB
+    acceptedFiles: "application/vnd.openxmlformats-officedocument.presentationml.presentation",
+    addRemoveLinks: true,
+    autoProcessQueue: false,
+    error: function (file, message) {
+        if (!file.accepted)
+            this.removeFile(file);
+        $("#message_alert").text(message);
+        $("#alert").attr("style", "");
+        setTimeout(function () {
+            $("#alert").fadeOut(700);
+        }, 3000);
+    },
+    accept: function (file, done) {
+        $('.dz-progress').hide();
+    },
+    init: function () {
+        var myDropzone = this;
+        $("#btn-convert").click(function () {
+            myDropzone.on("sending", function (file, xhr, formData) {
+                formData.append("output-type", $('input[name="output_type"]:checked').val());
+            });
+            $('.dz-progress').show();
+            myDropzone.processQueue();
+        });
+        $("#btn-cancel").click(function () {
+            myDropzone.removeAllFiles();
+        });
+    }
+};
+
+
