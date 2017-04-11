fbe4e597360b00e0889c575fd3ae496192fe05d3
Jonas Vandamme <jrovdamm.vandamme@ugent.be>
2017-04-12 00:16:27 +0200

    created webslidesConverterException

---
+++ OpenWebslidesConverter/src/openwebslidesconverter/WebslidesConverterException.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package openwebslidesconverter;
+
+/**
+ *
+ * @author Jonas
+ */
+public class WebslidesConverterException extends Exception {
+
+    WebslidesConverterException(String msg) {
+        super(msg);
+    }
+}
