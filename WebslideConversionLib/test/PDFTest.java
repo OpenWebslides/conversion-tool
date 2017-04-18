43382b15bdb0ad7f37ac0f7d03079d96512c2c86
kavdnhou <Karel.VandenHoute@UGent.be>
2017-04-18 09:31:21 +0200

    split tests

---
+++ WebslideConversionLib/test/PDFTest.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import conversion.ConverterFactory;
+import conversion.IConverter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import objects.PPT;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+
+/**
+ *
+ * @author Karel
+ */
+public class PDFTest {
+    
+     public static void main(String[] args) {
+        // TODO code application logic here
+        File file = new File("C:\\temp\\slides.pdf");
+        IConverter converter = ConverterFactory.getConverter(file);
+        PPT ppt = new PPT();
+        
+        converter.parse(ppt,"C:\\temp\\");
+        
+        
+	FileWriter fw = null;
+        //uitschrijfstuk nog niet van toepassing...
+        try {
+                fw = new FileWriter("C:\\temp\\test.html");
+               // fw.write(ppt.toHTML());
+        } catch (Exception e) {
+        } finally {
+            try {
+                fw.close();
+            } catch (IOException ex) {
+            }
+        }
+    }
+}
---
+++ WebslideConversionLib/test/PPTTest.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import conversion.ConverterFactory;
+import conversion.IConverter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import logger.Logger;
+import objects.PPT;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import output.StdLogOutput;
+
+/**
+ *
+ * @author Karel
+ */
+public class PPTTest {
+    
+    
+    @Test
+    public void testRun() {
+        File file = new File("C:\\temp\\pres1.pptx");
+
+        IConverter converter = ConverterFactory.getConverter(file);
+        converter.setOutput(new StdLogOutput(new Logger("c:\\temp\\log", "logging", "karel")));
+
+        PPT ppt = new PPT();
+
+        converter.parse(ppt, "C:\\temp\\images");
+
+        FileWriter fw = null;
+
+        try {
+            fw = new FileWriter("C:\\temp\\test.html");
+        } catch (Exception e) {
+        } finally {
+            try {
+                fw.close();
+            } catch (IOException ex) {
+            }
+        }
+
+    }
+}
