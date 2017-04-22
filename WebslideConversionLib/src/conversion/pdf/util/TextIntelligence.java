e1d5a52360da4db09df2e5864b65f4c0c05e306e
Joann Chan <joann.chan@ugent.be>
2017-04-22 18:58:22 +0200

    textparts are now grouped in text per line (pdf)

--- WebslideConversionLib/src/conversion/pdf/util/TextIntelligence.java
+++ WebslideConversionLib/src/conversion/pdf/util/TextIntelligence.java
@@ -5,7 +5,8 @@
  */
 package conversion.pdf.util;
 
-
+import java.util.ArrayList;
+import java.util.List;
 import objects.PPT;
 import objects.Slide;
 import objects.Text;
@@ -23,18 +24,33 @@ public class TextIntelligence {
          */
 
         for (Slide slide : ppt.getSlides()) {
-            for (int i = 0; i < slide.getPptObjects().size(); i++) {
-                try{
+            List<Integer> toRemove = new ArrayList<>();
+            int i = 0;
+            while (i < slide.getPptObjects().size()) {
                 if (slide.getPptObjects().get(i) != null && slide.getPptObjects().get(i) instanceof Textpart) {
                     Text text = new Text();
-                    Textpart tp = (Textpart) slide.getPptObjects().get(i);
-                    //System.out.println("Tekstpart behandeld: " + tp.getXPosition()+","+tp.getYPosition());
+                    Textpart tp = new Textpart((Textpart) slide.getPptObjects().get(i));
+                    tp.setContent(tp.getContent());
                     text.addTextpart(tp);
+                    double posY = tp.getYPosition();
+                    //System.out.println("Tekstpart behandeld: " + tp.getXPosition()+","+tp.getYPosition());
+                    int ii = i;
+                    while (ii + 1 < slide.getPptObjects().size() && slide.getPptObjects().get(ii + 1) instanceof Textpart && posY == ((Textpart) slide.getPptObjects().get(ii + 1)).getYPosition()) {
+                        tp = new Textpart((Textpart) slide.getPptObjects().get(ii + 1));
+                        tp.setContent(tp.getContent());
+                        text.addTextpart(tp);
+                        toRemove.add(ii + 1);
+                        ii++;
+                    }
                     slide.getPptObjects().set(i, text);
-                }}
-                catch(Error e){
-                    
+                    i = ii + 1;
+                } else {
+                    i++;
                 }
+            }
+            Object[] keys = toRemove.toArray();
+            for (int j = keys.length - 1; j >= 0; j--) {
+                slide.getPptObjects().remove((int) keys[j]);
             }
         }
 
